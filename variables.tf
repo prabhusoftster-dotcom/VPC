96c7ae994b47816b1885ce3c528b32ce1abe521a
prabhusoftster-dotcom <prabhusoftster@gmail.com>
2026-05-30 13:58:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+variable "public_subnet1_cidr" {
+  type = string
+}
+
+variable "public_subnet2_cidr" {
+  type = string
+}
